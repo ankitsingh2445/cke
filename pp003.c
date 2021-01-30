5132220e21decb1950304f687e955ed133db5032
ankitsingh2445 <75989708+ankitsingh2445@users.noreply.github.com>
2021-01-30 19:39:45 +0530

    Update pp003.c

--- pp003.c
+++ pp003.c
@@ -1,17 +1,23 @@
+//Write a program to input a number and check it to be even or odd using bitwise operator.
+
+
+
 #include<stdio.h>
 main()
 {
 	int n;
 	scanf("%d",&n);
 	if(n&1)
-    printf("entered number %d is odd",n);
+        printf("entered number %d is odd",n);
 	else
 	printf("entered number %d is even",n);
-	
-	
-	
-	
-
-	
 
 }
+
+
+// output of program.
+
+
+// input          output
+// 2              entered number 2 is even 
+// 17             entered number 17 is odd
